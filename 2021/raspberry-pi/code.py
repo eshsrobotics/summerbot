1b93bacf70c5e3754ea5ade642b1d5416d2e6e69
mgookin24 <mgookin_24@esusdstudents.org>
2022-07-24 13:19:22 -0700

    Committing experimental code for marker handling.

--- 2021/raspberry-pi/code.py
+++ 2021/raspberry-pi/code.py
@@ -6,6 +6,10 @@ import numpy as np
 import cv2 as cv
 import cv2.aruco as aruco
 
+ARU_PIXEL_SIZE = 300                               # This number is the size of the marker image in pixels.
+ARU_BORDER_WIDTH = 1                               # This is the width of the border (TODO: is this in pixels or grid cells?)
+ARU_DICT = aruco.Dictionary_get(aruco.DICT_4X4_50) # In order for aruco to work, we need to find the predefined dictionary.
+ARU_PARAM = aruco.DetectorParameters_create()
 
 # # Select a marker from the predefined dictionary and save it to a file.
 
@@ -58,15 +62,40 @@ def generate_aruco_marker(marker_id, file_name):
     - marker_id: This is the ID of the Aruco Marker to generate from DICT_4X4_50.
     - file_name: This is the name of the file which has the marker saved into it.
     """
-    ARU_PIXEL_SIZE = 300  # This number is the size of the marker image in pixels.
-    ARU_BORDER_WIDTH = 1 # This is the width of the border (TODO: is this in pixels or grid cells?)
-    # In order for aruco to work, we need to find the predefined dictionary.
-    ARU_DICT = aruco.Dictionary_get(aruco.DICT_4X4_50)
-    ARU_PARAM = aruco.DetectorParameters_create()
+
 
     # Writing the image for disk.
     marker_image = aruco.drawMarker(ARU_DICT, marker_id, ARU_PIXEL_SIZE)
     cv.imwrite(file_name, marker_image)
+
+def detect_aruco_markers(img, view_results: bool):
+    """
+    Detects all ArUco markers in the given image and returns interesting data about them.
+    
+    Arguments:
+    - img: The input image.
+    - view_results: If True, an interactive window will appear that shows the
+                    source image and the detected and rejected markers.
+
+    Returns:
+    - Returns a list of (id, x, y, z) tuples with:
+      * id: The ID of the marker that was successfully detected
+      * x: The horizontal position of the marker's center relative to the center
+           of the image.  Negative values are left of center.
+      * y: The vertical position of the marker's center relative to the center
+          of the image.  Negative values are above the center.
+      * z: The depth of the marker's center relative to the center of the image.
+           These values will always be positive.
+      
+      Together, (x, y, z) form a translation vector between the center of the image and the
+      center of the marker.
+    """
+    corners, ids, rejectedImgpoints = aruco.detectMarkers(img, ARU_DICT, parameters=ARU_PARAM)
+    
+    for i, corner in enumerate(corners):        
+        print(f"Marker {i}:")
+        for i, point in corner:
+            print(f"Corner {i}: x={point[0]}, y={point[1]}")    
 
 if __name__ == "__main__":
     parse_arguments()
